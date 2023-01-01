2f4e8652bccac6f90ce62ffd7d261e9df72ca403
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-01-01 12:57:48 -0300

    Utilizando o print  para mostrar na tela.

---
+++ olamundo.py
@@ -0,0 +1,4 @@
+print ('Ola,Mundo!')
+print ('Muito legal aprender esta linguagem de programacao!!')
+print ('Vai me ajudar a aprender coisas novas uteis e que vai me deixar rico')
+print ('Eu amo busca conhecimento neste mundo da tecnologia!')
